d163b8959ff108a512483215ed9790afd293c7a1
robertmarinescu <robert.marinescu93@gmail.com>
2022-03-09 18:17:56 +0200

    add POST method to add a a new anecdote to the server

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -2,15 +2,17 @@ import React from 'react'
 import {createAnecdote} from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 import {useSelector, useDispatch} from 'react-redux'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
 
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setNotification(`You created ${content}`))
     setTimeout(() => {
       dispatch(setNotification(''))
